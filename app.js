a32207e5838b8f91302f78cb100707bec7f68b4c
VINEET KUMAR RAJAK <vkrajak39@gmail.com>
2024-01-09 00:01:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const newheading = React.createElement("h1",{},"hello everyone fro react");
+console.log(newheading);
+
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(newheading);
